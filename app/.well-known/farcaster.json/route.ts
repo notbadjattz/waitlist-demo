44470e95244a2378f0f5ea82ac00e549eae8d3d7
notbadjattz <notbadjattz@gmail.com>
2025-10-04 20:21:43 +0530

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,3 +4,7 @@ import { minikitConfig } from "../../../minikit.config";
 export async function GET() {
   return Response.json(withValidManifest(minikitConfig));
 }
+
+  "baseBuilder": {
+    "allowedAddresses": ["0x818c0aAb63BC459d9C3961649dBA54767FCeaCF5"]
+  }
